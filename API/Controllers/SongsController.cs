041e354a7df62e0e7ff70fe8a7f8ecbab1779615
alyshapennypacker <alysha.c.pennypacker@gmail.com>
2024-08-31 15:15:36 -0700

    Checking in SongsController and DB Connections

---
+++ API/Controllers/SongsController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SongsController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public SongsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Song>> Get()
+        {
+            return await _context.Songs.ToListAsync();
+        }
+
+    }
+    //     [HttpGet("{id}")]
+    //     public async Task<Song> Get(int id)
+    //     {
+    //         return await _context.Songs.FindAsync(id);
+    //     }
+
+    //     [HttpPost]
+    //     public async Task<Song> Post(Song song)
+    //     {
+    //         _context.Songs.Add(song);
+    //         await _context.SaveChangesAsync();
+    //         return song;
+    //     }
+
+    //     [HttpPut("{id}")]
+    //     public async Task<Song> Put(int id, Song song)
+    //     {
+    //         _context.Entry(song).State = EntityState.Modified;
+    //         await _context.SaveChangesAsync();
+    //         return song;
+    //     }
+
+    //     [HttpDelete("{id}")]
+    //     public async Task Delete(int id)
+    //     {
+    //         var song = await _context.Songs.FindAsync(id);
+    //         _context.Songs.Remove(song);
+    //         await _context.SaveChangesAsync();
+    //     }
+    // }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,20 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["dbConnString"]);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
